0bcaecd09f4aaa3fbf5339b040059ca5af591454
Andre Macedo <Andre Macedo>
2016-05-24 18:22:19 +0100

    added web client

---
+++ WebClient/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WebClient
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                
+
+            }
+        }
+
+    }
+    }
+}
---
+++ WebClient/Global.asax.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebClient
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
