893ca636e638d5a33de6c5a0007d12e30571c715
Andre Macedo <Andre Macedo>
2016-05-26 17:41:33 +0100

    webbclient is now an external service

--- Informatics/Program.cs
+++ Informatics/Program.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.ServiceModel;
 
-namespace Server {
-  class Program {
-    static void Main(string[] args) {
-      ServiceHost host = new ServiceHost(typeof(InterBank.InterBankOps));
-      host.Open();
-      Console.WriteLine("Service InterBank Active. Press <Enter> to close.");
-      Console.ReadLine();
-      host.Close();
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            ServiceHost host = new ServiceHost(typeof(InterBank.InterBankOps));
+            host.Open();
+            Console.WriteLine("Service InterBank Active. Press <Enter> to close.");
+            Console.ReadLine();
+            host.Close();
+        }
     }
-  }
 }
