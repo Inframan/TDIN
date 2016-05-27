23e1e5e8905ae1a8149ec8ddcaff54cd844285e2
Inframan <ei12087@fe.up.pt>
2016-05-27 11:13:47 +0100

    prof melhor ajuda

---
+++ WebAppApi/IService1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebAppApi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [WebInvoke(Method = "POST", UriTemplate = "/purchaseStock", BodyStyle = WebMessageBodyStyle.WrappedRequest, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        void PurchaseStock(string company, int quantity, string username, string email, DateTime request_date_time, string execution_value, string order_type);
+
+        [WebGet(UriTemplate = "/companies", ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        string[] GetCompanies();
+
+        [WebInvoke(Method = "GET", UriTemplate = "/orders/{name}&{mail}", ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        string[][] GetOrders(string name, string mail);
+
+    }
+   
+}
---
+++ WebAppApi/RestService.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using WebAppApi;
+using WebAppApi.InterBank;
+
+namespace RestService
+{
+    public class RestService : IService1
+    {
+        InterBankOpsClient o;
+        public RestService()
+        {
+            o = new InterBankOpsClient();
+        }
+
+
+        public void PurchaseStock(string company, int quantity, string username, string email, DateTime request_date_time, string execution_value, string order_type)
+        {
+            o.PurchaseStock(company, quantity, username, email, request_date_time, execution_value, order_type);
+        }
+
+        public string[] GetCompanies()
+        {
+            return o.GetCompanies();
+        }
+
+        public string[][] GetOrders(string name, string mail)
+        {
+            return o.GetOrders(name, mail);
+        }
+    }
+}
